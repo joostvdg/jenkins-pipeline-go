e1e084dd707e6b3ffc2de9a4aaf5c57bf74b812a
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-01-28 16:50:18 +0100

    Utilize a docker container so we can always build golang

--- vars/goBuild.groovy
+++ vars/goBuild.groovy
@@ -1,24 +1,32 @@
 import com.github.joostvdg.Utilities
 
-def goHome = tool name: 'Go Latest', type: 'org.jenkinsci.plugins.golang.GolangInstallation'
-env.PATH = "${goHome}/bin:${env.PATH}"
-env.GOROOT = "${goHome}"
-
 def call() {
-
-    stage('SCM') {
-        checkout scm
-    }
-    stage('Build') {
-        sh 'go build src/main.go'
-    }
-    stage('Run') {
-        sh 'go run src/main.go'
-    }
 
     stage('LibExample') {
         def utils = new Utilities(steps)
         String name = 'Joost'
         utils.sayHello(name)
     }
+
+
+    stage('SCM') {
+        checkout scm
+    }
+
+    dir('src'){
+
+        docker.image('golang').inside {
+            stage('Build') {
+                sh 'go get -d -v -t && go build -v -o hello-world-go'
+            }
+        }
+        stage('Run') {
+            sh 'go run src/main.go'
+        }
+        stage('Archive') {
+            archive 'hello-world-go'
+        }
+    }
+
+
 }
